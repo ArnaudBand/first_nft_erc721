357c8efe6a824889b6e5a4f2cc4a99e490efb0f7
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2025-02-14 18:23:30 +0300

    feat: add deployment script for NFT contract

---
+++ script/DeployNft.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import { Script } from "forge-std/Script.sol";
+import { NFT } from "../src/NFT.sol";
+
+contract DeployNFT is Script {
+  uint256 deploperPrivateKey = vm.envUint("PRIVATE_KEY");
+  NFT public nft;
+
+  function run () external returns(NFT) {
+    vm.startBroadcast(deploperPrivateKey);
+    nft = new NFT("MEME NFT", "MN");
+    vm.stopBroadcast();
+
+    return nft;
+  }
+}
